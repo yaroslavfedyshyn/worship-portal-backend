34c47075269d8c5d8acc866d62d0a9516261f278
ko-yaroslav <korin.yaroslav@gmail.com>
2019-09-25 14:40:03 +0300

    Reset password Joi model config Change SMTP user

--- server/routes/auth/joiSchemas.js
+++ server/routes/auth/joiSchemas.js
@@ -19,7 +19,9 @@ const forgotPasswordSchema = Joi.object({
 });
 
 const resetPasswordSchema = Joi.object({
-  password: Joi.string().min(6).max(30).regex(/[a-zA-Z0-9]{6,30}/)
+  password: Joi.string().min().max(30).regex(/[a-zA-Z0-9]{6,30}/)
+    .required(),
+  token: Joi.string().min(30).max(30).regex(/[a-zA-Z0-9]{30,30}/)
     .required(),
 });
 
