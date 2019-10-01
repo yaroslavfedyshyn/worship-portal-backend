ab24ffb4d9018e96f59bcb46b039b815c80cb94c
yaroslavfedyshyn <fedyshyn.y@gmail.com>
2019-10-02 00:17:28 +0300

    Update schemaValidate.js

--- server/routes/auth/schemaValidate.js
+++ server/routes/auth/schemaValidate.js
@@ -2,22 +2,23 @@ const validator = require('express-joi-validation').createValidator({
   passError: true
 });
 
-module.exports = (joiSchema, options) => {
-  options = options || {
+module.exports = (joiSchema, options = {}) => {
+  const joiOptions = {
     abortEarly: false,
+    ...options,
   };
   const result = [];
 
   if (joiSchema.body) {
-     result.push(validator.body(joiSchema.body, {joi: options}))
+     result.push(validator.body(joiSchema.body, {joi: joiOptions}))
   }
 
   if (joiSchema.query) {
-    result.push(validator.query(joiSchema.query, {joi: options}))
+    result.push(validator.query(joiSchema.query, {joi: joiOptions}))
   }
 
   if (joiSchema.params) {
-    result.push(validator.params(joiSchema.params, {joi: options}))
+    result.push(validator.params(joiSchema.params, {joi: joiOptions}))
   }
 
   return result;
