e49057721ba40452ed793bc698a5e6606c137ac0
ko-yaroslav <korin.yaroslav@gmail.com>
2019-10-03 14:31:24 +0300

    Display _error property passed to SubmissionError fn in the form Forgot password form always returns 200

--- server/routes/auth/login/_post.js
+++ server/routes/auth/login/_post.js
@@ -1,52 +1,49 @@
 const Session = require('../../../models/session');
 const User = require('../../../models/user');
 const tokenGenerate = require('../../../utils/generateString');
-const { GENERATE_SESSION_TOKEN_LENGTH } = require('../../../constants');
+const {GENERATE_SESSION_TOKEN_LENGTH} = require('../../../constants');
 const createError = require('../../../utils/createError');
 
 module.exports = async (req, res, next) => {
-  const {
-    email,
-    password,
-  } = req.body;
+    const {
+        email,
+        password,
+    } = req.body;
 
-    const user = await User.findOne({ email });
+    const user = await User.findOne({email});
 
     if (!user) {
-      const errData = {
-        path:   ['email'],
-        type: 'not.found',
-        message: 'invalid email or password',
-        transKey: 'server.error.user.not.found'
-      };
+        const errData = {
+            generalMessage: 'not found',
+            transKey: 'server.error.user.not.found'
+        };
 
-      return res.status(422)
-          .json(createError(errData))
+        return res.status(422)
+            .json(createError(errData))
     }
 
-      user.comparePassword(password, async (err, isMatch) => {
+    user.comparePassword(password, async (err, isMatch) => {
         if (err) throw err;
-        if(!isMatch) {
-          const errData = {
-            path:   ['email'],
-            type: 'not.found',
-            message: 'invalid email or password',
-            transKey: 'server.error.user.not.found'
-          };
+        if (!isMatch) {
+            const errData = {
+                generalMessage: 'not found',
+                transKey: 'server.error.user.not.found'
+            };
 
-          return res.status(422)
-              .json(createError(errData))
+
+            return res.status(422)
+                .json(createError(errData))
 
         }
 
-          const session = new Session({
+        const session = new Session({
             token: tokenGenerate(GENERATE_SESSION_TOKEN_LENGTH),
             userId: user._id,
-          });
+        });
 
-          await session.save();
+        await session.save();
 
-          res.cookie('sessionToken', session.token, { maxAge: 900000, httpOnly: true })
-              .json(user);
-      });
+        res.cookie('sessionToken', session.token, {maxAge: 900000, httpOnly: true})
+            .json(user);
+    });
 };
